21e58164cb75774e867572a5ce1b2c57fc4c8baa
Peter Livesey <plivesey453@gmail.com>
2014-07-25 11:16:02 -0700

    Adding a new Xcode project

---
+++ JazzPractice/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  JazzPractice
+//
+//  Created by Peter Livesey on 7/25/14.
+//  Copyright (c) 2014 Peter Livesey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
